06214217c140d85d6c0a743b26255a1409e2f66a
anastasia-kaz <anastasia-kaz@mail.ru>
2025-11-23 14:09:42 +0300

    hw_18

---
+++ src/homework_18/runnable/Car.java
@@ -0,0 +1,56 @@
+package homework_18.runnable;
+
+public class Car implements Runnable{
+
+    private String carName;
+    private String number;
+    private int stopTime;
+
+    public Car(String carName, String number, int stopTime) {
+        this.carName = carName;
+        this.number = number;
+        this.stopTime = stopTime;
+    }
+
+    public String getCarName() {
+        return carName;
+    }
+
+    public void setCarName(String carName) {
+        this.carName = carName;
+    }
+
+    public String getNumber() {
+        return number;
+    }
+
+    public void setNumber(String number) {
+        this.number = number;
+    }
+
+    public int getStopTime() {
+        return stopTime;
+    }
+
+    public void setStopTime(int stopTime) {
+        this.stopTime = stopTime;
+    }
+
+    public void drive() {
+        for (int i = 0; i <= 5; i++){
+            System.out.println("машина " + carName + " c номером " + number + " едет");
+            try {
+                Thread.sleep(stopTime);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+                System.out.println("машина заглохла пока спал поток");
+            }
+        }
+    }
+
+
+    @Override
+    public void run() {
+        drive();
+    }
+}
